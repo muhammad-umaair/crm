ff2fdf1952a6215c8d7aae1dc50ce38f344349d7
Muhammad Umair <muhammad.umair@sleekhive.com>
2023-07-03 22:19:11 +0500

    Add: article api routes

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticlesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+Route::controller(ArticlesController::class)->group(function () {
+    Route::get('/articles', 'index');
+    Route::get('/articles/{id}', 'show');
+    Route::post('/articles', 'store');
+    Route::put('/articles/{id}', 'update');
+    Route::delete('/articles/{id}', 'destroy');
+});
